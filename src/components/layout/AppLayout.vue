0fa5de6a16848d44bc6bf888fa36bfc20e0bc440
GHINonan <giannonan@gmail.com>
2024-12-18 20:51:53 +0800

    Frontend Login Signup and Guard

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-responsive>
+    <v-app :theme="theme">
+      <v-app-bar class="px-3" color="blue-darken-4">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          text=""
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <slot name="content"></slot>
+      </v-main>
+
+      <v-footer class="font-weight-bold" color="blue-darken-4" elevation="24" border app
+        >2024 - Skill Hub</v-footer
+      >
+    </v-app>
+  </v-responsive>
+</template>
